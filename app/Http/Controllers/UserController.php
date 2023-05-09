8609d842206dbbb862098e7e9ac3260535b7a363
wpellas <william.pellas@hotmail.com>
2023-05-09 09:15:16 +0200

    cache

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Events\OurExampleEvent;
+use App\Models\Post;
 use App\Models\User;
 use App\Models\Follow;
 use Illuminate\Http\Request;
+use App\Events\OurExampleEvent;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 
@@ -19,7 +21,12 @@ class UserController extends Controller
                 'posts' => auth()->user()->feedPosts()->latest()->paginate(4)
             ]);
         } else {
-            return view('homepage');
+            $postCount = Cache::remember('postCount', 20, function () {
+                return Post::count();
+            });
+            return view('homepage', [
+                'postCount' => $postCount
+            ]);
         }
     }
 
